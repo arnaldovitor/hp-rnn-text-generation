22aba7a26d43d3d36bc925225bc2145e5e87fda9
Arnaldo Vitor <arnaldovitorbarros@gmail.com>
2022-04-12 22:08:51 -0300

    Embedding layer -> One-Hot

--- GRU.py
+++ GRU.py
@@ -5,13 +5,14 @@ class GRUModel(tf.keras.Model):
     def __init__(self, vocab_size, embedding_dim, rnn_units):
         super().__init__(self)
         self.vocab_size = vocab_size
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        # self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
         self.gru = tf.keras.layers.GRU(rnn_units, return_sequences=True, return_state=True)
         self.dense = tf.keras.layers.Dense(vocab_size)
 
     def call(self, inputs, states=None, return_state=False, training=False):
         x = inputs
-        x = self.embedding(x, training=training)
+        # x = self.embedding(x, training=training)
+        x = tf.one_hot(x, self.vocab_size)
 
         if states is None:
             states = self.gru.get_initial_state(x)
